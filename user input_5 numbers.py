cbe48f4d4e232892e3f72a9dbaf1e2572efb815d
viviennebeduya <viviennebeduya@gmail.com>
2024-10-15 21:30:53 +0800

    polished pseudocode, initialize user input

--- user input_5 numbers.py
+++ user input_5 numbers.py
@@ -1,13 +1,19 @@
-#set program's function
-    #identify which number is the highest
-    #    if number 1 is greater than number 2, set number 1 as highest
-    #    if number 2 is greater than number 1, set number 2 as highest
-    #compare the left numbers to the set highest number
+#set program's function to identify which is the highest number among the user's 5 inputs
+    #identify which number is the highest 
+    #    if number 1 is greater than number 2, set number 1 as the highest number
+    #    else, set number 2 as the highest number
+    #compare the remaining numbers to the current highest number
     #    if number 3 is greater than the highest number, set number 3 as the highest number
-    #        else, the previous highest number settles
+    #        else, the previous highest number remains
     #    if number 4 is greater than the highest number, set number 4 as the highest number
-    #        else, the previous highest number settles
-    #    if number 5 is greater than the highest number, set number 4 as the highest number
-    #        else, the previous highest number settles
-#initialize user's input from number1 to number5
-#print the highest number
+    #        else, the previous highest number remains
+    #    if number 5 is greater than the highest number, set number 5 as the highest number
+    #        else, the previous highest number remains
+    # return the highest number from the function
+#initialize user's input from number 1 to number 5
+num1 = int(input("Input first number: "))
+num2 = int(input("Input second number: "))
+num3 = int(input("Input third number: "))
+num4 = int(input("Input fourth number: "))
+num5 = int(input("Input last number: "))
+#print the function result
