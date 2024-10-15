b1d30d8f8b18e42750c2b3fb8a1f8d507c140b97
viviennebeduya <viviennebeduya@gmail.com>
2024-10-15 21:59:30 +0800

    ensure snake_case (changed numbers variable

--- user input_5 numbers.py
+++ user input_5 numbers.py
@@ -1,33 +1,33 @@
 #set program's function to identify which is the highest number among the user's 5 inputs
-def find_highest_number (number1,number2,number3,number4,number5):
+def find_highest_number (number_one,number_two,number_three,number_four,number_five):
     #identify which number is the highest 
     #    if number 1 is greater than number 2, set number 1 as the highest number
-    if number1 > number2:
-        highest_number = number1
+    if number_one > number_two:
+        highest_number = number_one
     #    else, set number 2 as the highest number
     else:
-        highest_number = number2
+        highest_number = number_two
     #compare the remaining numbers to the current highest number
     #    if number 3 is greater than the highest number, set number 3 as the highest number
-    #        else, the previous highest number remains
-    if number3 > highest_number:
-        highest_number = number3
+    #        else, the previous highest number remains 
+    if number_three > highest_number:
+        highest_number = number_three
     #    if number 4 is greater than the highest number, set number 4 as the highest number
     #        else, the previous highest number remains
-    if number4 > highest_number:
-        highest_number = number4
+    if number_four > highest_number:
+        highest_number = number_four
     #    if number 5 is greater than the highest number, set number 5 as the highest number
     #        else, the previous highest number remains
-    if number5 > highest_number:
-        highest_number = number5
+    if number_five > highest_number:
+        highest_number = number_five
     # return the highest number from the function
     return highest_number
 #initialize user's input from number 1 to number 5
-number1 = int(input("Input first number: "))
-number2 = int(input("Input second number: "))
-number3 = int(input("Input third number: "))
-number4 = int(input("Input fourth number: "))
-number5 = int(input("Input last number: "))
+number_one = int(input("Input first number: "))
+number_two = int(input("Input second number: "))
+number_three = int(input("Input third number: "))
+number_four = int(input("Input fourth number: "))
+number_five = int(input("Input last number: "))
 #print the function result
-result = find_highest_number (number1,number2,number3,number4,number5)
+result = find_highest_number (number_one,number_two,number_three,number_four,number_five)
 print (f"The highest number is {result}.")
