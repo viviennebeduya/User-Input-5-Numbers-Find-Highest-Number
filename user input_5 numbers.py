63bde2310fd0bf6b32ffb5c61d54b9c790a58a7b
viviennebeduya <viviennebeduya@gmail.com>
2024-10-15 21:11:07 +0800

    update pseudocode

--- user input_5 numbers.py
+++ user input_5 numbers.py
@@ -1,12 +1,13 @@
-#user input 5 numbers
-#identify which number is the highest
-#    if number 1 is greater than number 2, set number 1 as highest
-#    if number 2 is greater than number 1, set number 2 as highest
-#compare the left numbers to the set highest number
-#    if number 3 is greater than the highest number, set number 3 as the highest number
-#        else, the previous highest number settles
-#    if number 4 is greater than the highest number, set number 4 as the highest number
-#        else, the previous highest number settles
-#    if number 5 is greater than the highest number, set number 4 as the highest number
-#        else, the previous highest number settles
+#set program's function
+    #identify which number is the highest
+    #    if number 1 is greater than number 2, set number 1 as highest
+    #    if number 2 is greater than number 1, set number 2 as highest
+    #compare the left numbers to the set highest number
+    #    if number 3 is greater than the highest number, set number 3 as the highest number
+    #        else, the previous highest number settles
+    #    if number 4 is greater than the highest number, set number 4 as the highest number
+    #        else, the previous highest number settles
+    #    if number 5 is greater than the highest number, set number 4 as the highest number
+    #        else, the previous highest number settles
+#initialize user's input from number1 to number5
 #print the highest number
