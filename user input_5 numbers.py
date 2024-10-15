9dd16ce996cee479cd61026395e9225f40a2c1aa
viviennebeduya <viviennebeduya@gmail.com>
2024-10-15 20:45:31 +0800

    pseudocode draft

---
+++ user input_5 numbers.py
@@ -0,0 +1,12 @@
+#user input 5 numbers
+#identify which number is the highest
+#    if number 1 is greater than number 2, set number 1 as highest
+#    if number 2 is greater than number 1, set number 2 as highest
+#compare the left numbers to the set highest number
+#    if number 3 is greater than the highest number, set number 3 as the highest number
+#        else, the previous highest number settles
+#    if number 4 is greater than the highest number, set number 4 as the highest number
+#        else, the previous highest number settles
+#    if number 5 is greater than the highest number, set number 4 as the highest number
+#        else, the previous highest number settles
+#print the highest number
